58de7c93f24ceaf1a39dbb374cdd0e6afcac3e0d
koooommmm <nico.pika17@gmail.com>
2024-03-14 22:32:32 +0900

    update: 一覧ページを修正

--- frontend/src/components/InvoiceList.js
+++ frontend/src/components/InvoiceList.js
@@ -1,24 +1,67 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { invoices } from "../models/invoices";
 
 const InvoiceList = () => {
+  const navigate = useNavigate(); // useNavigateフックを使ってnavigate関数を取得
+
+  // 行をクリックしたときに実行する関数
+  const handleRowClick = (invoiceId) => {
+    navigate(`/invoice/${invoiceId}`); // 該当する請求書の詳細ページへ遷移
+  };
+
   return (
-    <div className="max-w-4xl mx-auto mt-10 p-5 shadow-lg rounded-lg">
-      <h1 className="text-xl font-bold mb-4">請求書一覧</h1>
-      <ul className="list-disc pl-5">
-        {invoices.map((invoice) => (
-          <li key={invoice.id} className="mb-2">
-            <Link
-              to={`/invoice/${invoice.id}`}
-              className="text-blue-500 hover:text-blue-700"
-            >
-              {invoice.companyName} - 合計金額:{" "}
-              {invoice.getTotalAmount().toLocaleString()}円
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="max-w-4xl mx-auto mt-10">
+      <div className="p-5 bg-white shadow-md rounded-lg">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold text-gray-700">請求書リスト</h1>
+          <Link
+            to="/create-invoice"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300"
+          >
+            請求書作成
+          </Link>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoices.map((invoice) => (
+                <tr
+                  key={invoice.id}
+                  className="bg-white border-b hover:bg-gray-50 cursor-pointer"
+                  onClick={() => handleRowClick(invoice.id)}
+                >
+                  <td>
+                    <text className="border-2 border-indigo-500 text-indigo-700 font-bold py-2 px-4 rounded">
+                      {invoice.status}
+                    </text>
+                  </td>
+                  <td className="py-4 px-6">
+                    <div className="text-xl">
+                      {invoice.companyName} 様<br />
+                    </div>
+                    {invoice.billingDate.toLocaleDateString()}
+                  </td>
+                  <td className="py-4 px-6">
+                    <div className="text-xl">
+                      {invoice.getTotalAmount().toLocaleString()}円 <br />
+                    </div>
+                    ({invoice.getTotalAmountWithTax().toLocaleString()}円)
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/models/Invoice.js
+++ frontend/src/models/Invoice.js
@@ -1,12 +1,26 @@
 export default class Invoice {
-  constructor(id, companyName, items) {
+  constructor(id, companyName, billingDate, dueDate, items, status = "未請求") {
     this.id = id;
     this.companyName = companyName;
-    this.items = items; // InvoiceItemのインスタンスの配列
+    this.billingDate = billingDate;
+    this.dueDate = dueDate;
+    this.items = items;
+    this.status = status;
   }
 
   // 合計金額を計算
   getTotalAmount() {
     return this.items.reduce((total, item) => total + item.amount, 0);
+  }
+
+  // 税込合計金額を計算（ここでは消費税率を10%とする）
+  getTotalAmountWithTax(taxRate = 0.1) {
+    const subtotal = this.getTotalAmount();
+    return subtotal + subtotal * taxRate;
+  }
+
+  // 請求状況ステータスを更新
+  updateStatus(newStatus) {
+    this.status = newStatus;
   }
 }
