e70a209c941e2d2e5f1f3a6c27828b36ae339bd6
koooommmm <nico.pika17@gmail.com>
2024-04-12 15:52:21 +0900

    fix: firebase設定を環境変数に切り出し

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWXHi-qfDde2CuAhk3cv2kvAYgmuyAAjA",
-  authDomain: "invoice-app-3f2c3.firebaseapp.com",
-  projectId: "invoice-app-3f2c3",
-  storageBucket: "invoice-app-3f2c3.appspot.com",
-  messagingSenderId: "289616041770",
-  appId: "1:289616041770:web:1f17a688e317ca5e4c3053",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
